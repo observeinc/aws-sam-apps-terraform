716b1d207631d843e6ac6416ff424e9fd52ac789
Wade Moore <wade@observeinc.com>
2025-05-30 17:00:47 -0400

    Add observe provider variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 #
-# The following variables MUST be set accordingly
+# Required variables for authenticating to Observe
 #
 
 variable "observe_workspace" {
@@ -8,19 +8,43 @@ variable "observe_workspace" {
   default     = "Default"
 }
 
+variable "observe_domain" {
+  description = "The name of the Observe workspace"
+  type        = string
+  default     = "observeinc.com"
+}
+
+variable "observe_customer_id" {
+  description = "The name of the Observe workspace"
+  type        = string
+}
+
+variable "observe_user" {
+  description = "The name of the Observe workspace"
+  type        = string
+}
+
+variable "observe_user_password" {
+  description = "The name of the Observe workspace"
+  type        = string
+}
+
+#
+# Required variables for configuring the integration
+#
+
 variable "organization_root_id" {
   description = "The ID of the AWS Organization root (e.g., r-xxxxxxxx)"
   type        = string
-  default     = "CHANGEME"
 }
 
 variable "aws_account_map" {
   description = "Map of AWS account IDs to lists of regions"
   type        = map(list(string))
-  default = {
-    "123456789011" = ["us-east-1", "us-east-2"]
-    "123456789012" = ["us-east-1", "us-west-2"]
-  }
+  # default = {
+  #   "123456789011" = ["us-east-1", "us-east-2"]
+  #   "123456789012" = ["us-east-1", "us-west-2"]
+  # }
 }
 
 variable "custom_aws_account_map" {
@@ -36,7 +60,6 @@ variable "custom_aws_account_map" {
 variable "aws_management_account" {
   description = "The ID of the AWS Organization Management Account"
   type        = string
-  default     = "123456789011"
 }
 
 
